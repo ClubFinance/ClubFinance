8ee7738449aca2f5424bfc064437b7aae537e301
Claudio Fleischmann <claudio@fleischmann.app>
2020-04-16 14:56:27 +0200

    Theme und Erstellung Home-Page

---
+++ public/index.js
@@ -0,0 +1,15 @@
+function delete_ajax(url, id, name) {
+    if(confirm('Bist du dir sicher, dass du den Eintrag "' + name + '" löschen möchtest?')) {
+        fetch(url + id, {
+            method: 'DELETE',
+        }).then(res => window.location.reload());
+    }
+}
+
+if(window.innerWidth <= 765) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    if ($(".sidebar").hasClass("toggled")) {
+        $('.sidebar .collapse').collapse('hide');
+    };
+}
