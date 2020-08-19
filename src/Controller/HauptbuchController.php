f49de6e0112494800717cdf244d09b5442fc59c3
Claudio Fleischmann <claudio@fleischmann.app>
2020-08-19 22:33:21 +0200

    Hauptbuch

--- src/Controller/HauptbuchController.php
+++ src/Controller/HauptbuchController.php
@@ -5,6 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Hauptbuch;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class HauptbuchController extends AbstractController
 {
     /**
@@ -12,8 +21,125 @@ class HauptbuchController extends AbstractController
      */
     public function index()
     {
+        $buchungssaetze = $this->getDoctrine()->getRepository(Hauptbuch::class)->findBy(array(), array('datum' => 'DESC'));
+
         return $this->render('hauptbuch/index.html.twig', [
-            'controller_name' => 'HauptbuchController',
+            'hauptbuch' => $buchungssaetze,
         ]);
+    }
+
+    /**
+     * @Route("/hauptbuch/new", name="hauptbuch_new")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request) {
+        $hauptbuch = new Hauptbuch();
+        
+        $form = $this->createFormBuilder($hauptbuch)
+            ->add('datum', DateType::class, [
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('soll', NumberType::class, [
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('haben', NumberType::class, [
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('betrag', MoneyType::class, [
+                'divisor' => 100,
+                'currency' => false,
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('beschreibung', TextareaType::class, [
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('beleg', TextType::class, [
+                'label' => 'Beleg(e)',
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Speichern',
+                'attr' => ['class' => 'btn btn-primary mt-3'],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() &&  $form->isValid()) {
+            $data = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($data);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Der Buchungssatz wurde erfasst.');
+        }
+
+        return $this->render('hauptbuch/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/hauptbuch/edit/{id}", name="hauptbuch_edit")
+     * Method({"GET", "POST"})
+     */
+    public function edit(Request $request, $id) {
+        $hauptbuch = $this->getDoctrine()->getRepository(Hauptbuch::class)->find($id);
+        
+        $form = $this->createFormBuilder($hauptbuch)
+            ->add('datum', DateType::class, [
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('soll', NumberType::class, [
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('haben', NumberType::class, [
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('betrag', MoneyType::class, [
+                'divisor' => 100,
+                'currency' => false,
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('beschreibung', TextareaType::class, [
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('beleg', TextType::class, [
+                'label' => 'Beleg(e)',
+                'attr' => ['class' => 'form-control mb-3']
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Speichern',
+                'attr' => ['class' => 'btn btn-primary mt-3'],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() &&  $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Der Buchungssatz wurde erfolgreich gespeichert.');
+        }
+
+        return $this->render('hauptbuch/edit.html.twig', [
+            'form' => $form->createView(),
+            'satz' => $hauptbuch,
+        ]);
+    }
+
+    /**
+     * @Route("hauptbuch/delete/{id}", name="hauptbuch_delete")
+     */
+    public function delete($id) {
+        $user = $this->getDoctrine()->getRepository(Hauptbuch::class)->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('hauptbuch');
     }
 }
