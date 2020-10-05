3cb1e52ebe08750424093d38e8cbfe37f71117c8
Claudio Fleischmann <claudio@fleischmann.app>
2020-10-05 14:39:37 +0200

    Anpassung Stichtag

--- src/Controller/BilanzController.php
+++ src/Controller/BilanzController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Kontenplan;
+use App\Entity\Hauptbuch;
 
 use App\Service\Kontostand;
 
@@ -92,6 +93,10 @@ class BilanzController extends AbstractController
         $passiven = $fk['sum'] + $ek['sum'];
 
 
+        // Letzter Buchungssatz (für Stichdatum Bilanz)
+        $stichtag = $this->getDoctrine()->getRepository(Hauptbuch::class)->findBy(array(), array('datum' => 'DESC'))[0];
+
+
         return $this->render('bilanz/index.html.twig', [
             'umlaufvermoegen' => $uv,
             'anlagevermoegen' => $av,
@@ -101,6 +106,43 @@ class BilanzController extends AbstractController
             'passiven' => $passiven,
             'abschluss' => $abschluss,
             'abschlussColor' => $abschluss_color,
+            'stichtag' => $stichtag,
+        ]);
+    }
+
+    /**
+     * @Route("/bilanz/export/pdf", name="bilanz_export_pdf")
+     */
+    public function export_pdf() {
+        $buchungssaetze = $this->getDoctrine()->getRepository(Hauptbuch::class)->findBy(array(), array('datum' => 'DESC'));
+
+        foreach($buchungssaetze as $satz) {
+            $sql = $this->getDoctrine()->getRepository(Kontenplan::class)->findBy(array("id4" => $satz->getSoll()))[0];
+            $satz->setSollT($satz->getSoll().' - '.$sql->getName());
+
+            $sql = $this->getDoctrine()->getRepository(Kontenplan::class)->findBy(array("id4" => $satz->getHaben()))[0];
+            $satz->setHabenT($satz->getHaben().' - '.$sql->getName());
+        }
+
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        $pdfOptions->set('isRemoteEnabled', TRUE);
+        
+        $dompdf = new Dompdf($pdfOptions);
+
+        $html = $this->renderView('hauptbuch/pdf.html.twig', [
+            'hauptbuch' => $buchungssaetze,
+        ]);
+        
+        $dompdf->loadHtml($html);
+        
+        $dompdf->setPaper('A4', 'landscape');
+
+        $dompdf->render();
+
+        $dompdf->stream(date("Y-m-d").'-Hauptbuch.pdf', [
+            "Attachment" => false
         ]);
     }
 }
