39c2acaefb4072d285c257aac6db0e3fa404dfe3
Claudio Fleischmann <claudio@fleischmann.app>
2020-10-26 17:29:32 +0100

    Plugins an/aus

--- src/Service/Plugins.php
+++ src/Service/Plugins.php
@@ -6,7 +6,9 @@ class Plugins {
     public function get() {
         $plugins = array();
 
-        $plugins[] = array(
+        $plugins['status'] = false; // Plugin Status
+
+        $plugins['plugins'][] = array(
             "name" => "Demo-Plugin",
             "route_name" => "demo_plugin",
         );
