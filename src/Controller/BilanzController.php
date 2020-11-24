c42c2fa15da945b1afab735bd8df9261784afda7
Claudio Fleischmann <claudio@fleischmann.app>
2020-11-24 18:24:44 +0100

    PDF-Export Bilanz & ER

--- src/Controller/BilanzController.php
+++ src/Controller/BilanzController.php
@@ -11,6 +11,10 @@ use App\Entity\Hauptbuch;
 
 use App\Service\Kontostand;
 
+// Include Dompdf required namespaces
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
 class BilanzController extends AbstractController
 {
     /**
@@ -121,17 +125,92 @@ class BilanzController extends AbstractController
     /**
      * @Route("/bilanz/export/pdf", name="bilanz_export_pdf")
      */
-    public function export_pdf() {
+    public function export_pdf(Kontostand $kontostand) {
         // -- Bilanz PDF-export
-        $buchungssaetze = $this->getDoctrine()->getRepository(Hauptbuch::class)->findBy(array(), array('datum' => 'DESC'));
+        $umlaufvermoegen = $this->getDoctrine()
+                                ->getRepository(Kontenplan::class)->createQueryBuilder('p')
+                                ->where('p.id2 = 10')->andWhere('p.id4 != 0')
+                                ->andWhere('p.status != 0')
+                                ->orderBy('p.id1', 'ASC')->addOrderBy('p.id2', 'ASC')->addOrderBy('p.id3', 'ASC')->addOrderBy('p.id4', 'ASC')
+                                ->getQuery()->getResult();
 
-        foreach($buchungssaetze as $satz) {
-            $sql = $this->getDoctrine()->getRepository(Kontenplan::class)->findBy(array("id4" => $satz->getSoll()))[0];
-            $satz->setSollT($satz->getSoll().' - '.$sql->getName());
+                                $uv = array();
 
-            $sql = $this->getDoctrine()->getRepository(Kontenplan::class)->findBy(array("id4" => $satz->getHaben()))[0];
-            $satz->setHabenT($satz->getHaben().' - '.$sql->getName());
+                                foreach($umlaufvermoegen as $i) {
+                                    $uv[$i->getName()] = $kontostand->get($i->getId4(),$this->getDoctrine());
+                                }
+
+                                $uv['sum'] = array_sum($uv);
+
+        $anlagevermoegen = $this->getDoctrine()
+                                ->getRepository(Kontenplan::class)->createQueryBuilder('p')
+                                ->where('p.id2 = 14')->andWhere('p.id4 != 0')
+                                ->andWhere('p.status != 0')
+                                ->orderBy('p.id1', 'ASC')->addOrderBy('p.id2', 'ASC')->addOrderBy('p.id3', 'ASC')->addOrderBy('p.id4', 'ASC')
+                                ->getQuery()->getResult();
+
+                                $av = array();
+
+                                foreach($anlagevermoegen as $i) {
+                                    $av[$i->getName()] = $kontostand->get($i->getId4(),$this->getDoctrine());
+                                }
+
+                                $av['sum'] = array_sum($av);
+
+        $fremdkapital = $this->getDoctrine()
+                                ->getRepository(Kontenplan::class)->createQueryBuilder('p')
+                                ->where('p.id2 = 20')->orWhere('p.id2 = 24')->andWhere('p.id4 != 0')
+                                ->andWhere('p.status != 0')
+                                ->orderBy('p.id1', 'ASC')->addOrderBy('p.id2', 'ASC')->addOrderBy('p.id3', 'ASC')->addOrderBy('p.id4', 'ASC')
+                                ->getQuery()->getResult();
+
+                                $fk = array();
+
+                                foreach($fremdkapital as $i) {
+                                    $fk[$i->getName()] = $kontostand->get($i->getId4(),$this->getDoctrine());
+                                }
+
+                                $fk['sum'] = array_sum($fk);
+
+        $eigenkapital = $this->getDoctrine()
+                                ->getRepository(Kontenplan::class)->createQueryBuilder('p')
+                                ->where('p.id2 = 28')->andWhere('p.id4 != 0')
+                                ->andWhere('p.status != 0')
+                                ->orderBy('p.id1', 'ASC')->addOrderBy('p.id2', 'ASC')->addOrderBy('p.id3', 'ASC')->addOrderBy('p.id4', 'ASC')
+                                ->getQuery()->getResult();
+
+                                $ek = array();
+
+                                foreach($eigenkapital as $i) {
+                                    $ek[$i->getName()] = $kontostand->get($i->getId4(),$this->getDoctrine());
+                                }
+
+                                $ek['sum'] = array_sum($ek);
+
+        $aktiven = $uv['sum'] + $av['sum'];
+        $passiven = $fk['sum'] + $ek['sum'];
+
+        if($aktiven > $passiven) {
+            // Gewinn
+            $abschluss = 'Gewinn';
+            $abschluss_color = 'success';
+        } else {
+            // Verlust
+            $abschluss = 'Verlust';
+            $abschluss_color = 'danger';
         }
+
+        // Gewinn/Verlust berechnen und in EK einfügen
+        $sum = $ek['sum'];
+        unset($ek['sum']);
+        $differenz = $aktiven - $passiven;
+        $ek[$abschluss] = $differenz;
+        $ek['sum'] = $sum + $differenz;
+        $passiven = $fk['sum'] + $ek['sum'];
+
+
+        // Letzter Buchungssatz (für Stichdatum Bilanz)
+        $stichtag = $this->getDoctrine()->getRepository(Hauptbuch::class)->findBy(array(), array('datum' => 'DESC'))[0];
 
         // Configure Dompdf according to your needs
         $pdfOptions = new Options();
@@ -140,17 +219,27 @@ class BilanzController extends AbstractController
         
         $dompdf = new Dompdf($pdfOptions);
 
-        $html = $this->renderView('hauptbuch/pdf.html.twig', [
-            'hauptbuch' => $buchungssaetze,
+        // Design PDF
+        $html = $this->renderView('bilanz/pdf.html.twig', [
+            'umlaufvermoegen' => $uv,
+            'anlagevermoegen' => $av,
+            'fremdkapital' => $fk,
+            'eigenkapital' => $ek,
+            'aktiven' => $aktiven,
+            'passiven' => $passiven,
+            'abschluss' => $abschluss,
+            'abschlussColor' => $abschluss_color,
+            'stichtag' => $stichtag,
         ]);
         
         $dompdf->loadHtml($html);
         
-        $dompdf->setPaper('A4', 'landscape');
+        $dompdf->setPaper('A4', 'portrait');
 
         $dompdf->render();
 
-        $dompdf->stream(date("Y-m-d").'-Hauptbuch.pdf', [
+        // Ausgabe PDF
+        $dompdf->stream(date("Y-m-d").'-Bilanz.pdf', [
             "Attachment" => false
         ]);
     }
--- src/Controller/ErfolgsrechnungController.php
+++ src/Controller/ErfolgsrechnungController.php
@@ -11,6 +11,10 @@ use App\Entity\Hauptbuch;
 
 use App\Service\Kontostand;
 
+// Include Dompdf required namespaces
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
 class ErfolgsrechnungController extends AbstractController
 {
     /**
@@ -76,6 +80,89 @@ class ErfolgsrechnungController extends AbstractController
             'abschluss' => $abschluss,
             'abschlussColor' => $abschluss_color,
             'stichtag' => $stichtag,
+        ]);
+    }
+
+    /**
+     * @Route("/erfolgsrechnung/export/pdf", name="erfolgsrechnung_export_pdf")
+     */
+    public function export_pdf(Kontostand $kontostand) {
+        // -- Erfolgsrechnung als PDF exportieren
+        $aufwand = $this->getDoctrine()
+                        ->getRepository(Kontenplan::class)->createQueryBuilder('p')
+                        ->where('p.id1 = 4')->orWhere('p.id1 = 5')->orWhere('p.id1 = 6')
+                        ->andWhere('p.status != 0')
+                        ->orderBy('p.id1', 'ASC')->addOrderBy('p.id2', 'ASC')->addOrderBy('p.id3', 'ASC')->addOrderBy('p.id4', 'ASC')
+                        ->getQuery()->getResult();
+
+                        $aw = array();
+
+                        foreach($aufwand as $i) {
+                            $aw[$i->getName()] = $kontostand->get($i->getId4(),$this->getDoctrine());
+                        }
+
+                        $aw['sum'] = array_sum($aw);
+
+        $ertrag = $this->getDoctrine()
+                        ->getRepository(Kontenplan::class)->createQueryBuilder('p')
+                        ->where('p.id1 = 3')->orWhere('p.id1 = 7')->orWhere('p.id1 = 8')
+                        ->andWhere('p.status != 0')
+                        ->orderBy('p.id1', 'ASC')->addOrderBy('p.id2', 'ASC')->addOrderBy('p.id3', 'ASC')->addOrderBy('p.id4', 'ASC')
+                        ->getQuery()->getResult();
+
+                        $et = array();
+
+                        foreach($ertrag as $i) {
+                            $et[$i->getName()] = $kontostand->get($i->getId4(),$this->getDoctrine());
+                        }
+
+                        $et['sum'] = array_sum($et);
+
+        if($et['sum'] > $aw['sum']) {
+            // Gewinn
+            $abschluss = 'Gewinn';
+            $abschluss_color = 'success';
+        } else {
+            // Verlust
+            $abschluss = 'Verlust';
+            $abschluss_color = 'danger';
+        }
+
+        // Gewinn/Verlust berechnen und bei Ertrag einfügen
+        $sum = $aw['sum'];
+        unset($aw['sum']);
+        $differenz = $et['sum'] - $sum;
+        $aw[$abschluss] = $differenz;
+        $aw['sum'] = $sum + $differenz;
+
+        // Letzter Buchungssatz (für Stichdatum Erfolgsrechnung)
+        $stichtag = $this->getDoctrine()->getRepository(Hauptbuch::class)->findBy(array(), array('datum' => 'DESC'))[0];
+
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        $pdfOptions->set('isRemoteEnabled', TRUE);
+        
+        $dompdf = new Dompdf($pdfOptions);
+
+        // PDF Design
+        $html = $this->renderView('erfolgsrechnung/pdf.html.twig', [
+            'aufwand' => $aw,
+            'ertrag' => $et,
+            'abschluss' => $abschluss,
+            'abschlussColor' => $abschluss_color,
+            'stichtag' => $stichtag,
+        ]);
+        
+        $dompdf->loadHtml($html);
+        
+        $dompdf->setPaper('A4', 'portrait');
+
+        $dompdf->render();
+
+        // PDF ausgeben
+        $dompdf->stream(date("Y-m-d").'-Erfolgsrechnung.pdf', [
+            "Attachment" => false
         ]);
     }
 }
